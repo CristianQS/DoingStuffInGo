521f914635b5a94f9e7a2501529f764bd764999b
CristianQS <cristian_31teror@hotmail.com>
2021-04-06 23:13:50 +0100

    start cobra project

---
+++ Cobra/cmd/beers-cli/go.mod
@@ -0,0 +1,1 @@
+module "beers-cli"
---
+++ Cobra/cmd/beers-cli/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"cmd/cli"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.Execute()
+}
